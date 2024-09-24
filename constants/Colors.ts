aad245c507d2de769260ddf174b2292c0b836980
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2024-09-24 20:30:44 +0700

    define some constant color

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,19 +1,7 @@
-const tintColorLight = '#2f95dc';
-const tintColorDark = '#fff';
-
-export default {
-  light: {
-    text: '#000',
-    background: '#fff',
-    tint: tintColorLight,
-    tabIconDefault: '#ccc',
-    tabIconSelected: tintColorLight,
-  },
-  dark: {
-    text: '#fff',
-    background: '#000',
-    tint: tintColorDark,
-    tabIconDefault: '#ccc',
-    tabIconSelected: tintColorDark,
-  },
-};
+// define constant colors
+export default  {
+    primaryColor: '#ff7f36',
+    bgcolor:'#f4f4f4',
+    black:'#27283a',
+    white:'#ffffff'
+}
