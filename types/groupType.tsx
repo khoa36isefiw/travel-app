47042ba3710a04f3c661ed3e0c01540cfc694dfe
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2024-09-25 12:28:22 +0700

    define interface gor listing and groups

---
+++ types/groupType.tsx
@@ -0,0 +1,7 @@
+export interface IGroupType {
+  id: string;
+  name: string;
+  image: string;
+  rating: number;
+  reviews: number;
+}
---
+++ types/listingType.tsx
@@ -0,0 +1,11 @@
+export interface IListingType {
+  id: string;
+  name: string;
+  image: string;
+  description: string;
+  rating: number;
+  price: string;
+  duration: string;
+  location: string;
+  category: string;
+}
